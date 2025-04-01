92ad88136d8349d1e4ff7aff44c18c14bb953d5f
Xi Lu <lucyluxi2001@gmail.com>
2025-04-01 18:31:30 +1100

    Modified Home Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="about">
       <h1>About Our Library</h1>
-      <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+      <p>
+        Welcome to our digital library! We're dedicated to providing a vast
+        collection of books and resources to our community.
+      </p>
     </div>
- </template>
+  </template>
+  
+  <style scoped>
+  .about {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+    padding: 0 20px;
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,3 +4,15 @@
       <p>Welcome</p>
     </div>
   </template>
+  
+  <style scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+  }
+  </style>
+  
